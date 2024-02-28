bbe7da9bc47ef68839534c72b3030987a995a135
sanatd33 <saisanat@gmail.com>
2024-02-28 15:35:56 -0500

    Initial ui testing

---
+++ src/lib/key.svelte
@@ -0,0 +1,39 @@
+<script>
+    let keyProps = {class:[$$restProps.class]}
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<key on:mousedown
+    on:mouseup
+    on:click
+    {...keyProps}>
+    <slot/>
+</key>
+
+<style>
+    .white{
+        width: 5rem;
+        cursor: pointer;
+        position: relative;
+        height: 20rem;
+        border-radius: 10px;
+        border: 1px solid #000;
+        background-color: #fff;
+        display: inline-block;
+    }
+    .black{
+        cursor: pointer;
+        position: relative;
+        width: 2rem;
+        height: 13rem;
+        border-radius: 5px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        background-color: #000;
+        z-index: 2;
+        margin: 0 -20px 0 -20px;
+        display: inline-block;
+        border: 1px solid #fff;
+        bottom:7rem
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,137 @@
+<h1>ChordGen</h1>
+<script lang="ts">
+    import { onMount, tick } from "svelte";
+    import * as Tone from "tone";
+    import Key from "$lib/key.svelte";
+    let synthConst : Tone.Sampler;
+    let notes : string[] = [];
+    let prevNotes : string[] = [];
+    onMount(()=> {
+        Tone.start();
+        synthConst = new Tone.Sampler({
+            urls: {
+                "C4": "c4.wav",
+                "D#4": "ds4.wav",
+                "F#4": "fs4.wav",
+                "A4": "a4.wav"
+            },
+            baseUrl: "src/lib/"
+        }).toDestination();
+
+        setInterval(() => {
+            if (!arraysEqual(notes, prevNotes)) {
+                // synthConst.triggerAttack(notes);
+                prevNotes = [...notes];
+            }
+        }, 16);
+    })
+
+    function arraysEqual(a : any[], b : any[]) {
+        if (a == b) return true;
+        if (a == null || b == null) return false;
+        if (a.length != b.length) return false;
+
+        for (var i = 0; i < a.length; i++) {
+            if (a[i] !== b[i]) return false;
+        }
+        return true;
+    }
+
+</script>
+<Key class="white" on:mousedown={()=>{
+    synthConst.triggerAttack("C4");
+    notes.push("C4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="black" on:mousedown={()=>{
+    notes.push("C#4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="white" on:mousedown={()=>{
+    notes.push("D4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="black" on:mousedown={()=>{
+    notes.push("D#4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="white" on:mousedown={()=>{
+    notes.push("E4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="white" on:mousedown={()=>{
+    notes.push("F4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="black" on:mousedown={()=>{
+    notes.push("F#4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="white" on:mousedown={()=>{
+    notes.push("G4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="black" on:mousedown={()=>{
+    notes.push("G#4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="white" on:mousedown={()=>{
+    notes.push("A4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="black" on:mousedown={()=>{
+    notes.push("A#4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="white" on:mousedown={()=>{
+    notes.push("B4");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<Key class="white" on:mousedown={()=>{
+    notes.push("C5");
+}} on:mouseup={()=>{
+    //synth.triggerRelease();
+}}>
+</Key>
+
+<button on:click={()=>{
+    notes = [];
+}}>
+    Reset
+</button>
